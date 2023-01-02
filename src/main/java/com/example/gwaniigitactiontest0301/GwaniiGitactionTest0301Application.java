bcdb71d467b99646cd439759cb7755353f179316
강명관 <gwanii@gangmyeong-gwan-ui-MacBookPro.local>
2023-01-02 17:59:23 +0900

    Initial commit

---
+++ src/main/java/com/example/gwaniigitactiontest0301/GwaniiGitactionTest0301Application.java
@@ -0,0 +1,13 @@
+package com.example.gwaniigitactiontest0301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwaniiGitactionTest0301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwaniiGitactionTest0301Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gwaniigitactiontest0301/GwaniiGitactionTest0301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gwaniigitactiontest0301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwaniiGitactionTest0301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
